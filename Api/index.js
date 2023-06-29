926dfee1c498c274f902f6ffba85908e75929f26
Andres Riera <andresriera9999@gmail.com>
2023-06-29 16:29:49 -0300

    commitbeforelast

--- Api/index.js
+++ Api/index.js
@@ -14,8 +14,10 @@
 //    console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
 //  });
 const server = require("./src/app");
+const { conn } = require("./src/DB_connection");
 const PORT = 3001;
 
 server.listen(PORT, () => {
+  conn.sync({ force: true });
   console.log("Server raised in port: " + PORT);
 });
---
+++ Api/src/models/Favorite.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Favorite",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM("Alive", "Dead", "unknown"),
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
